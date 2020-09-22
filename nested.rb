bbabf045b01012d301ec917a19bbe99ade0ada70
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-09-22 14:26:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,9 +81,6 @@ def adding_matz
 end
 
 def changing_alan
-  # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-  # return the entire updated hash
-
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -122,5 +119,5 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 end
